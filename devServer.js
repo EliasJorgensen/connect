30949b814eaefa631f17eda9684f09de4e746a97
Elias Jørgensen <eliaszakarias@gmail.com>
2016-03-29 22:08:11 +0200

    Started work on rewriting parts of the webapp for use with redux.js

--- devServer.js
+++ devServer.js
@@ -4,7 +4,7 @@ var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
-  hot: true,
+  hot: true, // this is the important part for hot reloading the components
   historyApiFallback: true
 }).listen(8080, 'localhost', function (err, result) {
   if (err) {
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ var webpack           = require('webpack'),
     HtmlWebpackPlugin = require('html-webpack-plugin'),
     ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+// configure HtmlWebpackPlugin
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
 
+// export webpack config
 module.exports = {
   devtool: 'eval',
   entry: [
@@ -18,18 +20,20 @@ module.exports = {
   ],
   output: {
     path: __dirname + '/dist',
-    filename: "app.js"
+    filename: "bundle.js"
   },
   resolve: {
     modulesDirectories: ['node_modules', './app'],
     extensions: ['', '.js', '.jsx']
   },
+  // declare loaders to be used in webpack
   module: {
     loaders: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] },
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] }, //
       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') }
     ]
   },
+  // initialize the added webpack plugins
   plugins: [
     new ExtractTextPlugin('app.css'),
     HTMLWebpackPluginConfig,
